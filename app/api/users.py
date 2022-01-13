822eec80d0a27e6db72a7b315f8a67c207da7686
JacobG03 <jaqobek1995@gmail.com>
2022-01-13 14:04:55 +0100

    public user endpoints

--- app/api/users.py
+++ app/api/users.py
@@ -1,9 +1,14 @@
+import io
 from typing import List
 from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
+from starlette import status
 
 from app import crud
-from app.schemas import _users
+from app.models import Avatar
+from app.schemas import _users, _assets
 from app.api.deps import get_db
 
 router = APIRouter()
@@ -18,3 +23,43 @@ async def Get_Users(
 
     db_users = crud.user.get_multi(db, reverse=reverse, limit=limit, skip=skip)
     return db_users
+
+
+@router.get('/{username}', response_model=_users.UserBase)
+async def Get_User(
+        username: str,
+        db: Session = Depends(get_db)):
+
+    db_user = crud.user.get_by_username(db, username)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    return db_user
+
+
+@router.get('/{username}/avatar')
+async def Get_User(
+        username: str,
+        db: Session = Depends(get_db)):
+
+    db_user = crud.user.get_by_username(db, username)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    db_avatar = db.query(Avatar).get(db_user.avatar_id)
+
+    return StreamingResponse(io.BytesIO(db_avatar.content), media_type='image/png')
+
+
+@router.get('/{username}/theme', response_model=_assets.Theme)
+async def Get_Theme(
+        username: str,
+        db: Session = Depends(get_db)):
+
+    db_user = crud.user.get_by_username(db, username)
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    db_theme = crud.theme.get(db, model_id=db_user.theme_id)
+
+    return db_theme
