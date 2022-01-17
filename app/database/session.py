19f6eb47a66e916383225274c2762d89b78a9786
JacobG03 <jaqobek1995@gmail.com>
2022-01-17 17:55:15 +0100

    prod changes

--- app/database/session.py
+++ app/database/session.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from app.core import settings
 
 
-if 'sqlite' in settings.SQLALCHEMY_DATABASE_URI:
-    engine = create_engine(settings.SQLALCHEMY_DATABASE_URI,
-                           connect_args={"check_same_thread": False})
-else:
-    engine = create_engine(
-        settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+# if 'sqlite' in settings.SQLALCHEMY_DATABASE_URI:
+#     engine = create_engine(settings.SQLALCHEMY_DATABASE_URI,
+#                            connect_args={"check_same_thread": False})
+# else:
+engine = create_engine(
+    settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
